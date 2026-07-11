e265ef6b8a3814295d91d06b21857cda1e4e5f8c
Vermos1821 <miguelitomosquera01@gmail.com>
2026-07-11 12:21:52 -0500

    dev_Vera (#5)
    * UML actualizado
    
    * Create Messi

--- src/main/java/com/absolutecinema/utils/Paths.java
+++ src/main/java/com/absolutecinema/utils/Paths.java
@@ -1,8 +1,8 @@
 package com.absolutecinema.utils;
 
 public class Paths {
-    public static final String MOVIES_REPOSITORY = "src/main/resources/com/absolutecinema/data/peliculas.txt";
-    public static final String USER_REPOSITORY = "src/main/resources/com/absolutecinema/data/usuarios.txt";
+    public static final String MOVIES_REPOSITORY = "src/main/resources/com/absolutecinema/data/movies.txt";
+    public static final String USER_REPOSITORY = "src/main/resources/com/absolutecinema/data/users.txt";
     public static final String MENU_VIEW = "/com/absolutecinema/views/client/Menu.fxml";
     public static final String PELICULA_CARD_VIEW = "/com/absolutecinema/views/client/MovieCard.fxml";
     public static final String PELICULA_PROX_VIEW = "/com/absolutecinema/views/client/MovieReleaseCard.fxml";
