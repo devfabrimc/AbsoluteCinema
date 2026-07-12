3547c32911c126949248889c7eaa4da6d4defa55
Legnar <angel27casillas@gmail.com>
2026-07-12 15:56:30 -0500

    Implementación de la lógica del MovieService

--- src/main/java/com/absolutecinema/service/MovieService.java
+++ src/main/java/com/absolutecinema/service/MovieService.java
@@ -1,9 +1,10 @@
 package com.absolutecinema.service;
 
 import com.absolutecinema.model.Movie;
+import com.absolutecinema.model.Genre;
+import com.absolutecinema.model.MovieStatus;
 import com.absolutecinema.repository.MovieRepository;
 
-import java.util.Collections;
 import java.util.List;
 
 public class MovieService {
@@ -13,25 +14,35 @@ public class MovieService {
     public MovieService(MovieRepository movieRepository){
         this.movieRepository = movieRepository;
     }
+
     //Metodo que devuelve la lista de películas en cartelera
     public List<Movie> getNowShowing(){
-        return Collections.emptyList();
+        return movieRepository.findByStatus(MovieStatus.NOW_SHOWING);
     }
+
     //Metodo que devuelve la lista de películas a estrenarse
     public List<Movie> getComingSoon(){
-        return Collections.emptyList();
+        return movieRepository.findByStatus(MovieStatus.COMING_SOON);
     }
-    //Metodo que busca una película por el nombre
-    public List<Movie> searchByTitle(String text){
-        return Collections.emptyList();
+
+    //Metodo que busca películas por el nombre
+    public List<Movie> searchByTitle(String title){
+        return movieRepository.findByTitle(title);
     }
-    //Metodo que busca una película por el id
+
+    //Metodo que busca películas por el género
+    public List<Movie> filterByGenre(Genre genre){
+        return movieRepository.findByGenre(genre);
+    }
+
+    //Metodo que busca una película por el ID
     public Movie getMovieById(String id){
-        return null;
+        return movieRepository.findById(id);
     }
+
     //Metodo que devuelve una lista con todas las películas
     public List<Movie> getAllMovies(){
-        return Collections.emptyList();
+        return movieRepository.findAll();
     }
 
 }
